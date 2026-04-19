e625b1b8bbb557c07b8614eb1590b64520a36118
impossible589 <91239042+impossible589@users.noreply.github.com>
2026-04-19 21:35:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
-
 from fastapi import FastAPI, WebSocket,File ,UploadFile, Request
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+prompt = ""
+infoforweb = {}
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="."), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:5500"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 
-
+)
 
 
 
 needtotalk = False
 @app.get("/item")
 async def get_item1():
-    prompt = "hello"
-    return {
+    global prompt
+    if(prompt==""):
+       return {"task":"null"}
+    else:
+        return {
 
-        "task": "shell", "value": str(prompt)
+        "task": "shell", "prompt": str(prompt)
 
     }
 
 
 
-@app.get("/main_handler")
+@app.get("/front_handler")
 async def get_item2(parcel: str):
+    global infoforweb
     print("Yes")
     print("parcel__" + parcel)
+    global prompt
+
     parcellist = parcel.split("-")
+    print(parcellist)
     if parcellist[0] == "shell":
        prompt = parcellist[1]
        print(prompt)
+       return {
+           "info":"okay"
+
+       }
+
+
     elif parcellist[0] == "statuscheck":
-        pass
+        print("statuscheck pinged"+str(infoforweb))
+        treturn = infoforweb
+        infoforweb = {}
+        return treturn
+
     elif parcellist[0] == "givefilename":
         pass
 
@@ -46,23 +73,31 @@ async def get_item3(parcel: str, price: int):
     elif parcellist[0] == "fileready":
         pass
 
-@app.post("/item")
+@app.post("/items")
 async def receive_item(data: dict):
-    print("Received:", data)
+    info = data.get("info")
+    print("infooo"+info)
+    global infoforweb
+    if info == "diffus_completed":
+        infoforweb = {"info" :"imageisready"}
 
-    if data.get("task"):
-        return {"status": "matched", "received": data}
+    if info == "diffus_progress":
+        progess = data.get("progress")
+        infoforweb = {"info" :"diffus_progress","value":progess}
+        print("updatedinfo4web"+str(infoforweb))
+
 
     return {"status": "no task"}
 
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
+    global infoforweb
     # Save file in chunks (important for large files)
     with open(file.filename, "wb") as f:
         while chunk := await file.read(1024 * 1024):  # 1MB chunks
             f.write(chunk)
-
+    infoforweb = "imageisready"
     return {"filename": file.filename, "status": "uploaded"}
 
 
